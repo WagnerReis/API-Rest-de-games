6d7ef932a25ac48e82169355268f3eb5de59644d
WagnerReis <wagnerreis1997@gmail.com>
2021-06-21 14:24:00 -0300

    cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const Game = require("./Game");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
