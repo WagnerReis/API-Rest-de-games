bcc0a6bdb8598829b39209293f7575c0de08bef3
WagnerReis <wagnerreis1997@gmail.com>
2021-06-22 11:31:49 -0300

    Geração de token

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const Game = require("./Game");
 const User = require("./User");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const JWTSecret = "kdfkjsadjkjksdabbjksadbjkdabkmnk";
 
 app.use(cors());
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -124,25 +126,32 @@ app.post("/auth",(req, res) => {
   var { email, password } = req.body;
 
   if(email != undefined){
-    var user = User.findOne({ where: {
+    User.findOne({ where: {
       email: email
-    }});
+    }}).then(user=>{
+      if(user != undefined){
+        if(user.password == password){
 
-    if(user != undefined){
-
-      if(user.password == password){
-        res.status = 200;
-        res.json({token: "TOKEN FALSO "});
+          jwt.sign({id: user.id, email: user.email},JWTSecret,{expiresIn:'48h'},(err, token) => {
+            if(err){
+              res.status(400);
+              res.json({err: "Falha interna"});
+            }else{
+              res.status(200);
+              res.json({token: token});
+            }
+          });
+        }else{
+          res.status(401);
+          res.json({ err: "Credenciais inválidas!"});
+        }
       }else{
-        res.status = 401;
-        res.json({ err: "Credenciais inválidas!"});
+        res.status(404);
+        res.json({ err: "O E-mail enviado não existe na base de dados!"});
       }
-    }else{
-      res.status = 404;
-      res.json({ err: "O E-mail enviado não existe na base de dados!"});
-    }
+  });
   }else{
-    res.status = 400;
+    res.status(400);
     res.json({ err: "O E-mail enviado é inválido"});
   }
 });
