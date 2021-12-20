6115822c7e9036470abdd409440db112f01124f2
“WagnerReis” <wagnerreis1997@gmail.com>
2021-12-19 22:50:49 -0300

    fix

--- index.js
+++ index.js
@@ -11,140 +11,153 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-function auth(req, res, next){
-  const authToken = req.headers['authorization'];
+function auth(req, res, next) {
+  const authToken = req.headers["authorization"];
 
-  if(authToken != undefined){
-    const bearer = authToken.split(' ');
+  if (authToken != undefined) {
+    const bearer = authToken.split(" ");
     var token = bearer[1];
 
     jwt.verify(token, JWTSecret, (err, data) => {
-      if(err){
+      if (err) {
         res.status(401);
-        res.json({err: "Token inválido!"});
-      }else{
+        res.json({ err: "Token inválido!" });
+      } else {
         req.token = token;
-        req.loggedUser = {id: data.id, email: data.email};
+        req.loggedUser = { id: data.id, email: data.email };
         next();
       }
-    })
-  }else{
+    });
+  } else {
     res.status(401);
-    res.json({err:"Token inválido!"});
+    res.json({ err: "Token inválido!" });
   }
 }
 
 app.get("/games", auth, (req, res) => {
   res.statusCode = 200;
-  Game.findAll().then(games => {
+  Game.findAll().then((games) => {
     res.json(games);
   });
 });
 
 app.get("/game/:id", auth, (req, res) => {
-  if(isNaN(req.params.id)){
+  if (isNaN(req.params.id)) {
     res.sendStatus(400);
-  }else{
+  } else {
     var id = parseInt(req.params.id);
 
     var HATEOAS = [
       {
-        href: "http://localhost:456789/game/"+id,
+        href: "http://localhost:456789/game/" + id,
         method: "DELETE",
-        rel: "delete_game"
+        rel: "delete_game",
       },
       {
-        href: "http://localhost:456789/game/"+id,
+        href: "http://localhost:456789/game/" + id,
         method: "PUT",
-        rel: "edit_game"
+        rel: "edit_game",
       },
       {
-        href: "http://localhost:456789/game/"+id,
+        href: "http://localhost:456789/game/" + id,
         method: "GET",
-        rel: "get_game"
+        rel: "get_game",
       },
       {
         href: "http://localhost:456789/game/",
         method: "GET",
-        rel: "get_all_games"
+        rel: "get_all_games",
       },
-    ]
-    Game.findByPk(id).then(game => {
-      if(game != undefined) {
-            res.statusCode = 200;
-            res.json({game, _links: HATEOAS});
-      }else{
+    ];
+    Game.findByPk(id)
+      .then((game) => {
+        if (game != undefined) {
+          res.statusCode = 200;
+          res.json({ game, _links: HATEOAS });
+        } else {
+          res.sendStatus(404);
+        }
+      })
+      .catch((err) => {
         res.sendStatus(404);
-      }
-    }).catch(err => {
-      res.sendStatus(404);
-    });
+      });
   }
 });
 
 app.post("/game", auth, (req, res) => {
-  var { title, price, year } = req.body; 
+  var { title, price, year } = req.body;
 
   Game.create({
     title,
     price,
-    year
-  }).then(() => {
-    res.sendStatus(200);
-  }).catch(() => {
-    res.sendStatus(404);
-  });
+    year,
+  })
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(() => {
+      res.sendStatus(404);
+    });
 });
 
 app.delete("/game/:id", auth, (req, res) => {
-  if(isNaN(req.params.id)){
+  if (isNaN(req.params.id)) {
     res.sendStatus(400);
-  }else{
+  } else {
     var id = parseInt(req.params.id);
-    Game.findByPk(id).then(game => {
-      if(game != undefined) {
-        Game.destroy({
-          where: { id: id }
-        }).then(() => {
-          res.sendStatus(200);
-        });
-      }else{
-        res.sendStatus(404);
-      }
-    }).catch(err => {
-      res.sendStatus(404);
-    });
-  }
-}); 
-
-app.put("/game/:id", auth, (req, res) => {
-  if(isNaN(req.params.id)){
-    res.sendStatus(400);
-  }else{
-    var id = parseInt(req.params.id);
-
-    Game.findByPk(id).then(game => {
-      if(game != undefined) {
-        var { title, price, year } = req.body;  
-          
-          Game.update({ title: title, price: price, year: year }, {
-            where: { id: id }
+    Game.findByPk(id)
+      .then((game) => {
+        if (game != undefined) {
+          Game.destroy({
+            where: { id: id },
           }).then(() => {
             res.sendStatus(200);
-          }).catch(err => {
-            res.sendStatus(404);
-          } );
-      }else{
+          });
+        } else {
+          res.sendStatus(404);
+        }
+      })
+      .catch((err) => {
         res.sendStatus(404);
-      }
-    }).catch(err => {
-      res.sendStatus(404);
-    });
+      });
+  }
+});
+
+app.put("/game/:id", auth, (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
+  } else {
+    var id = parseInt(req.params.id);
+
+    Game.findByPk(id)
+      .then((game) => {
+        if (game != undefined) {
+          var { title, price, year } = req.body;
+
+          Game.update(
+            { title: title, price: price, year: year },
+            {
+              where: { id: id },
+            }
+          )
+            .then(() => {
+              res.sendStatus(200);
+            })
+            .catch((err) => {
+              res.sendStatus(404);
+            });
+        } else {
+          res.sendStatus(404);
+        }
+      })
+      .catch((err) => {
+        res.sendStatus(404);
+      });
   }
 });
 
 app.get("/users", auth, (req, res) => {
-  User.findAll().then( users => {
+  User.findAll().then((users) => {
     res.json(users);
   });
 });
@@ -152,18 +165,20 @@ app.get("/users", auth, (req, res) => {
 app.post("/users/create", auth, (req, res) => {
   var { name, email, password } = req.body;
 
-  User.findOne({where:{email: email}}).then( user => {
-    if(user == undefined){
+  User.findOne({ where: { email: email } }).then((user) => {
+    if (user == undefined) {
       User.create({
         name: name,
         email: email,
-        password: password
-      }).then(() => {
-        res.sendStatus(200);
-      }).catch(() => {
-        res.sendStatus(400);
-      });
-    }else{
+        password: password,
+      })
+        .then(() => {
+          res.sendStatus(200);
+        })
+        .catch(() => {
+          res.sendStatus(400);
+        });
+    } else {
       res.sendStatus(404);
     }
   });
@@ -172,34 +187,40 @@ app.post("/users/create", auth, (req, res) => {
 app.post("/auth", (req, res) => {
   var { email, password } = req.body;
 
-  if(email != undefined){
-    User.findOne({ where: {
-      email: email
-    }}).then(user=>{
-      if(user != undefined){
-        if(user.password == password){
-
-          jwt.sign({id: user.id, email: user.email},JWTSecret,{expiresIn:'48h'},(err, token) => {
-            if(err){
-              res.status(400);
-              res.json({err: "Falha interna"});
-            }else{
-              res.status(200);
-              res.json({token: token});
+  if (email != undefined) {
+    User.findOne({
+      where: {
+        email: email,
+      },
+    }).then((user) => {
+      if (user != undefined) {
+        if (user.password == password) {
+          jwt.sign(
+            { id: user.id, email: user.email },
+            JWTSecret,
+            { expiresIn: "48h" },
+            (err, token) => {
+              if (err) {
+                res.status(400);
+                res.json({ err: "Falha interna" });
+              } else {
+                res.status(200);
+                res.json({ token: token });
+              }
             }
-          });
-        }else{
+          );
+        } else {
           res.status(401);
-          res.json({ err: "Credenciais inválidas!"});
+          res.json({ err: "Credenciais inválidas!" });
         }
-      }else{
+      } else {
         res.status(404);
-        res.json({ err: "O E-mail enviado não existe na base de dados!"});
+        res.json({ err: "O E-mail enviado não existe na base de dados!" });
       }
-  });
-  }else{
+    });
+  } else {
     res.status(400);
-    res.json({ err: "O E-mail enviado é inválido"});
+    res.json({ err: "O E-mail enviado é inválido" });
   }
 });
 
